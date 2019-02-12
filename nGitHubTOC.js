7a2d76d4c02a00255ac6c6fc128ab45fc2593e2b
IMTheNachoMan <imthenachoman@gmail.com>
2019-02-12 10:25:21 -0500

    Create nGitHubTOC.js

---
+++ nGitHubTOC.js
@@ -0,0 +1,42 @@
+function tocIt(inputMD, minHeading, maxHeading, ignoreLinex)
+{
+    if(minHeading > maxHeading) return;
+    
+    inputMDLines = inputMD.split("\n");
+    var outputMD = "";
+    var anchorTracker = {};
+    
+    for(var i = 0; i < inputMDLines.length; ++i)
+    {
+        var inputMDLine = inputMDLines[i];
+        var match = /^(#+) (.*)$/.exec(inputMDLine);
+        if(match)
+        {
+            var headingLevel = match[1].length;
+            var headingTitle = match[2].replace(/<.*?>/g, "");
+            
+            if(headingLevel < minHeading || headingLevel > maxHeading)
+            {
+                continue;
+            }
+            
+            headingLevel -= minHeading;
+            
+            var headingAnchor = headingTitle.toLowerCase().replace(/[^a-z0-9- ]/g, "").replace(/ /g, "-");
+            
+            if(headingAnchor in anchorTracker)
+            {
+                anchorTracker[headingAnchor]++;
+                headingAnchor = headingAnchor + "-" + anchorTracker[headingAnchor];
+            }
+            else
+            {
+                anchorTracker[headingAnchor] = 0;
+            }
+            
+            outputMD += " ".repeat(headingLevel * 2) + "- [" + headingTitle + "](#" + headingAnchor + ")\n";
+        }
+    }
+    
+    return outputMD;
+}
