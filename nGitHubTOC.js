881310341e1818a4410080a0d94aafb7f6fd3745
Leo-Paul Geneau <leoplo.lpg@nexedi.com>
2020-10-08 12:00:29 +0200

    adds accentuated characters support

--- nGitHubTOC.js
+++ nGitHubTOC.js
@@ -22,7 +22,7 @@ function tocIt(inputMD, minHeading, maxHeading, ignoreLinex)
             
             headingLevel -= minHeading;
             
-            var headingAnchor = headingTitle.toLowerCase().replace(/[^_a-z0-9- ]/g, "").replace(/ /g, "-");
+            var headingAnchor = headingTitle.toLowerCase().replace(/[^_0-9a-z\xE0-\xFF- ]/g, "").replace(/ /g, "-");
             
             if(headingAnchor in anchorTracker)
             {
