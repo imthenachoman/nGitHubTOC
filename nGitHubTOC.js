31180b245d858bc265f27d5de9d217a48195ce3b
IMTheNachoMan <imthenachoman@gmail.com>
2023-08-05 15:10:38 -0400

    Update nGitHubTOC.js
    trim heading line

--- nGitHubTOC.js
+++ nGitHubTOC.js
@@ -9,7 +9,7 @@ function tocIt(inputMD, minHeading, maxHeading, ignoreLinex)
 
     for(var i = 0; i < inputMDLines.length; ++i)
     {
-        var inputMDLine = inputMDLines[i];
+        var inputMDLine = inputMDLines[i].trim();
 
         var codeTag = /^.*(`{3}) ?(.*)?$/.exec(inputMDLine);
         if(codeTag)
